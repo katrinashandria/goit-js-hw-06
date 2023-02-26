a961586ffc154458fd4719247c15fcaebf623fd7
Kateryna Andriiash <pavlenko69@gmail.com>
2023-02-26 15:15:48 +0000

    JS6

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+
+const itemsList = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
+
+ulEl.append(...itemsList);
